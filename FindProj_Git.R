210053b8f6475a7917676c05d1ea5e29dddd22a6
colam2021 <onlam.study@gmail.com>
2024-06-16 17:43:46 -0700

    June 16 commit

---
+++ FindProj_Git.R
@@ -0,0 +1,5 @@
+print ("In Git: Move to the dir with project in it")
+
+print ("In the pc, go to the project folder first")
+
+print ("Then right click the folder, then Git will locate it properly")
